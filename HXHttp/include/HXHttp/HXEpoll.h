7c29364384aa93a3cf736bac3fc86aeb3b85031a
Heng_Xin <282000500@qq.com>
2024-07-18 14:59:27 +0800

    封装了Epoll的run, 正在架构多线程和处理可能的TCP粘包?~

--- HXHttp/include/HXHttp/HXEpoll.h
+++ HXHttp/include/HXHttp/HXEpoll.h
@@ -20,16 +20,21 @@
 #ifndef _HX_HXEPOLL_H_
 #define _HX_HXEPOLL_H_
 
-#include <sys/epoll.h>  // Epoll
+#include <sys/epoll.h> // Epoll
 
 namespace HXHttp {
 
 class HXEpoll {
-    int _epollFd;                  // epoll实例文件描述符
+    int _epollFd {-1};             // epoll实例文件描述符
+    int _serverFd {-1};            // 服务器套接字
     int _maxConnect;               // 最大连接数
-    int _serverFd;                 // 服务器套接字
     struct ::epoll_event _ev;      // epoll实例 事件
     struct ::epoll_event *_events; // 用户的 epoll 事件
+
+    /**
+     * @brief 报错, 并且释放Fd
+     */
+    void doError();
 
     // epoll_wait
     /**
@@ -38,8 +43,8 @@ class HXEpoll {
      *          -1 阻塞等待;
      *          0  立即返回;
      *          正数 阻塞等待的最长时间(单位: 毫秒)
-     * @return 失败时返回 -1，并设置 errno 错误码 ;
-     *        == 0 无消息;
+     * @return 失败时返回 -1, 并设置 errno 错误码;
+     *         == 0 无消息;
      *         > 0 有对应的消息数量
      */
     int wait(int timeOut);
@@ -48,28 +53,28 @@ class HXEpoll {
     /**
      * @brief 添加事件到epoll红黑树中
      * @param fd 文件操作符
-     * @param ev 指向 epoll_event 结构体的指针，用于指定事件类型和数据
-     * @return 成功时返回 0；失败时返回 -1，并设置 errno 错误码 
+     * @param ev 指向 epoll_event 结构体的指针, 用于指定事件类型和数据
+     * @return 成功时返回 0; 失败时返回 -1, 并设置 errno 错误码 
      */
-    int ctl_add(int fd);
+    int ctlAdd(int fd);
 
     // del
     /**
      * @brief 删除epoll红黑树中的事件
      * @param fd 文件操作符
-     * @param ev 指向 epoll_event 结构体的指针，用于指定事件类型和数据
-     * @return 成功时返回 0；失败时返回 -1，并设置 errno 错误码 
+     * @param ev 指向 epoll_event 结构体的指针, 用于指定事件类型和数据
+     * @return 成功时返回 0; 失败时返回 -1, 并设置 errno 错误码 
      */
-    int ctl_del(int fd);
+    int ctlDel(int fd);
 
     // mod (Modify)
     /**
      * @brief 修改epoll红黑树中的事件
      * @param fd 文件操作符
-     * @param ev 指向 epoll_event 结构体的指针，用于指定事件类型和数据
-     * @return 成功时返回 0；失败时返回 -1，并设置 errno 错误码 
+     * @param ev 指向 epoll_event 结构体的指针, 用于指定事件类型和数据
+     * @return 成功时返回 0; 失败时返回 -1, 并设置 errno 错误码 
      */
-    int ctl_mod(int fd);
+    int ctlMod(int fd);
 
 public:
     /**
@@ -79,6 +84,8 @@ public:
      * @param maxConnect 最大连接数 | 只是一个提示, 实际上, 内核会根据需要动态调整大小
      */
     explicit HXEpoll(int port = 28205, int maxQueue = 512, int maxConnect = 1);
+
+    void run();
 
     ~HXEpoll();
 };
--- HXHttp/src/HXHttp/HXEpoll.cpp
+++ HXHttp/src/HXHttp/HXEpoll.cpp
@@ -20,18 +20,18 @@ HXEpoll::HXEpoll(int port, int maxQueue, int maxConnect) : _maxConnect(maxConnec
         }
 
         // 初始化服务器的网络配置
-        struct sockaddr_in st_sersock;                  // 声明 IPv4 地址结构体
-        memset(&st_sersock, 0, sizeof(st_sersock));     // 初始化
-        st_sersock.sin_family = AF_INET;                // IPv4协议
-        st_sersock.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY 转换过来就是0.0.0.0，泛指本机的意思，也就是表示本机的所有IP，因为有些机子不止一块网卡，多网卡的情况下，这个就表示所有网卡ip地址的意思。
-        st_sersock.sin_port = htons(port);              // 端口
+        struct sockaddr_in stSersock;                  // 声明 IPv4 地址结构体
+        memset(&stSersock, 0, sizeof(stSersock));      // 初始化
+        stSersock.sin_family = AF_INET;                // IPv4协议
+        stSersock.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY 转换过来就是0.0.0.0，泛指本机的意思，也就是表示本机的所有IP，因为有些机子不止一块网卡，多网卡的情况下，这个就表示所有网卡ip地址的意思。
+        stSersock.sin_port = htons(port);              // 端口
 
         // 设置端口复用
         int opt = 1;
         setsockopt(_serverFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
         // 绑定
-        if (::bind(_serverFd, (struct sockaddr *)&st_sersock, sizeof(st_sersock)) < 0) { // 将套接字绑定IP和端口用于监听
+        if (::bind(_serverFd, (struct sockaddr *)&stSersock, sizeof(stSersock)) < 0) { // 将套接字绑定IP和端口用于监听
             LOG_ERROR("bind Error: %s (errno: %d)", strerror(errno), errno);
             break;
         }
@@ -62,7 +62,16 @@ HXEpoll::HXEpoll(int port, int maxQueue, int maxConnect) : _maxConnect(maxConnec
         LOG_INFO("====== Epoll已启动: \033[33m\033]8;;http://localhost:%d/\033\\http://localhost:%d/\033]8;;\033\\\033[0m\033[1;32m ======", port, port);
         return;
     } while (0);
-    exit(-1); // 错误处理
+    doError();
+}
+
+void HXEpoll::doError() {
+    if (_serverFd != -1)
+        ::close(_serverFd);
+    if (_epollFd != -1)
+        ::close(_epollFd);
+    LOG_ERROR("Repeat: Error: %s (errno: %d)", strerror(errno), errno);
+    exit(EXIT_FAILURE); // 错误处理
 }
 
 HXEpoll::~HXEpoll() {
@@ -76,20 +85,56 @@ int HXEpoll::wait(int timeOut) {
     return ::epoll_wait(_epollFd, _events, _maxConnect, timeOut);
 }
 
-int HXEpoll::ctl_add(int fd) {
+int HXEpoll::ctlAdd(int fd) {
     // --- 默认模式 --- | 那个高速模式我还不会?
     _ev.events = EPOLLIN;
     _ev.data.fd = fd;
     return ::epoll_ctl(_epollFd, EPOLL_CTL_ADD, fd, &_ev);
 }
 
-int HXEpoll::ctl_del(int fd) {
+int HXEpoll::ctlDel(int fd) {
     return ::epoll_ctl(_epollFd, EPOLL_CTL_DEL, fd, NULL);
 }
 
-int HXEpoll::ctl_mod(int fd) {
+int HXEpoll::ctlMod(int fd) {
     // --- 封装不确定 ---
     return ::epoll_ctl(_epollFd, EPOLL_CTL_MOD, fd, &_ev);
+}
+
+void HXEpoll::run() {
+    while (1) {
+        int nfds = wait(-1);
+        if (nfds == -1) {
+            LOG_ERROR("wait Error: %s (errno: %d)", strerror(errno), errno);
+            doError();
+        }
+
+        for (int i = 0; i < nfds; ++i) {
+            int tmpFd = _events[i].data.fd;
+            if (tmpFd == _serverFd) { // 新连接
+                int newClientFd = ::accept(_serverFd, (struct sockaddr *)NULL, NULL);
+                if (newClientFd < 0) {
+                    LOG_ERROR("连接新客户端出错! %s (errno: %d)", strerror(errno), errno);
+                } else {
+                    LOG_INFO("[INFO]: 欢迎新的客户机连接! id = %d", newClientFd);
+
+                    if (ctlAdd(newClientFd) < 0) {
+                        LOG_ERROR("添加时候出现错误! %s (errno: %d)", strerror(errno), errno);
+                        ::close(newClientFd);
+                    }
+                }
+            } else { // 处理新来的信息
+                int strLen = ::recv(tmpFd, msg, MAX_SIZE, 0);
+                if (strLen < 0) {
+                    LOG_ERROR("接收客户端[%d]消息出错: Error: %s (errno: %d)", tmpFd, strerror(errno), errno);
+                } else if (strLen == 0 && !(errno == EWOULDBLOCK || errno == EAGAIN)) {
+                    // 断开
+                    ctlDel(tmpFd);
+                    ::close(tmpFd);
+                }
+            }
+        }
+    }
 }
 
 } // namespace HXHttp
