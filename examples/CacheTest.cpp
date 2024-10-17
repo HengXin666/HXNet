dc2bcf86cd516820c9fbf56cfcccb87759ed9bb7
HengXin666 <282000500@qq.com>
2024-10-17 23:58:47 +0800

    新增了LRUCache的emplace方法, 但是有问题, 不是很能原地构造。。。

--- examples/CacheTest.cpp
+++ examples/CacheTest.cpp
@@ -1,16 +1,70 @@
 #ifdef CACHE_TEST_MAIN
 #include <HXSTL/container/LRUCache.hpp>
+#include <HXprint/print.h>
 
-HX::STL::container::LRUCache<int, int> testGetURLCache() {
-    HX::STL::container::LRUCache<int, int> tmp(3);
+struct Node {
+    int abc;
+    int cba;
+
+    Node(int a, int b)
+        : abc(a)
+        , cba(b)
+    {
+        HX::print::print("new: ", a, ", ", b);
+    }
+
+    Node(const Node& node)
+        : Node(node.abc, node.cba)
+    {
+        HX::print::print("copy!! (", abc, ", ", cba, ")");
+    }
+
+    Node& operator=(const Node& other) {
+        if (this != &other) {
+            // 拷贝成员变量
+            this->abc = std::move(other.abc);
+            this->cba = std::move(other.cba);
+        }
+        HX::print::print("awa");
+        return *this;
+    }
+
+
+    // Node& operator=(Node&&) = default;
+    // Node& operator=(const Node&) = delete;
+
+    void print() const {
+        HX::print::print(abc, cba);
+    }
+
+    ~Node() {
+        HX::print::print(">>>> End: ", abc, ", ", cba);
+    }
+};
+
+HX::STL::container::LRUCache<int, Node> testGetURLCache() {
+    HX::STL::container::LRUCache<int, Node> tmp(1);
     return tmp;
 }
 
+auto wdf() {
+    HX::STL::container::LRUCache<int, Node> lruCache = testGetURLCache();
+    lruCache.emplace(1, 1, 2);
+    auto& item = lruCache.get(1);
+    HX::print::print(item);
+    lruCache.emplace(2, 4, 5);
+    lruCache.emplace(3, 8, 9);
+    HX::print::print("size = ", lruCache.size());
+    HX::print::print(item);
+
+    std::list<Node> sb;
+    sb.emplace(sb.end(), 22, 33);
+    return item;
+}
+
 int main() {
-    HX::STL::container::LRUCache<int, int> lruCache = testGetURLCache();
-    lruCache.put(1, 2);
-    lruCache.put(1, 3);
-    
+    auto&& op = wdf();
+    HX::print::print(op);
     return 0;
 }
 
--- include/HXSTL/container/LRUCache.hpp
+++ include/HXSTL/container/LRUCache.hpp
@@ -22,7 +22,7 @@
 
 #include <list>
 #include <unordered_map>
-#include <optional>
+#include <stdexcept>
 
 namespace HX { namespace STL { namespace container {
 
@@ -33,13 +33,10 @@ namespace HX { namespace STL { namespace container {
  */
 template <class K, class V>
 class LRUCache {
+public:
     using ListType = std::pair<K, V>;
     using ListIterator = std::list<ListType>::iterator;
 
-    std::list<ListType> _cacheList;
-    std::unordered_map<K, ListIterator> _cacheMap;
-    std::size_t _capacity;
-public:
     LRUCache(std::size_t capacity) noexcept
         : _cacheList()
         , _cacheMap()
@@ -58,7 +55,7 @@ public:
     LRUCache& operator=(LRUCache&) = delete;
 
     LRUCache& operator=(LRUCache&& that) noexcept {
-        if (this != &that) {  // 防止自赋值
+        if (this != &that) [[likely]] {  // 防止自赋值
             _cacheList = std::move(that._cacheList);
             _cacheMap = std::move(that._cacheMap);
             _capacity = that._capacity;
@@ -68,27 +65,29 @@ public:
     }
     
     /**
-     * @brief 获取键`key`对应的值, 如果不存在则返回`std::nullopt`
+     * @brief 获取键`key`对应的值, 如果不存在则`抛出异常`
      * @param key 
-     * @return std::optional<V> 
+     * @return V 
+     * @throw std::range_error(键: 不存在)
+     * @warning 值得注意的是, 因为返回的是引用, 所以请尽早的使用, 防止悬挂引用! (缓存开大点)
      */
-    std::optional<V> get(const K& key) {
+    const V& get(const K& key) const {
         if (auto it = _cacheMap.find(key); it != _cacheMap.end()) {
-            _cacheList.splice(_cacheList.begin(), _cacheList, _cacheMap[key]);
+            _cacheList.splice(_cacheList.begin(), _cacheList, it->second);
             return _cacheList.begin()->second;
         }
-        return std::nullopt;
+        throw std::range_error("There is no such key in cache");
     }
     
     /**
-     * @brief 新增一个键值对, 如果有相同的则会覆盖旧的
+     * @brief 插入一个键值对, 如果有相同的则会覆盖旧的
      * @param key 
      * @param value 
      */
-    void put(const K& key, const V& value) {
+    void insert(const K& key, const V& value) {
         if (auto it = _cacheMap.find(key); it != _cacheMap.end()) {
             // 修改
-            _cacheList.splice(_cacheList.begin(), _cacheList, _cacheMap[key]);
+            _cacheList.splice(_cacheList.begin(), _cacheList, it->second);
             _cacheList.begin()->second = value;
         } else {
             // 添加
@@ -97,10 +96,41 @@ public:
                 _cacheMap.erase(_cacheList.rbegin()->first);
                 _cacheList.pop_back();
             }
-            // _cacheMap[key] = _cacheList.insert(_cacheList.begin(), {key, value});
             _cacheMap.emplace(key, _cacheList.emplace(_cacheList.begin(), key, value));
         }
     }
+
+    // TODO 是否有实现 emplace 的必要?
+    template <class... Args>
+    void emplace(const K& key, Args&&... args) {
+        if (auto it = _cacheMap.find(key); it != _cacheMap.end()) {
+            // 修改现有元素
+            _cacheList.splice(_cacheList.begin(), _cacheList, it->second);
+            // 原地构造
+            auto& value = _cacheList.begin()->second;
+            value.~V(); // 显式调用析构函数
+            new (&value) V(std::forward<Args>(args)...); // 原地构造
+        } else {
+            if (_cacheMap.size() == _capacity) {
+                _cacheMap.erase(_cacheList.rbegin()->first);
+                _cacheList.pop_back();
+            }
+            _cacheMap.emplace(key, _cacheList.emplace(_cacheList.begin(), key, V(std::forward<Args>(args)...)));
+        }
+    }
+
+    std::size_t size() const noexcept {
+        return _cacheMap.size();
+    }
+
+    bool empty() const noexcept {
+        return _cacheMap.empty();
+    }
+
+private:
+    mutable std::list<ListType> _cacheList;
+    std::unordered_map<K, ListIterator> _cacheMap;
+    std::size_t _capacity;
 };
 
 }}} // namespace HX::STL::container
